aed170aac2e6e3a380a0189980c9535a66428b40
Marco Massarotto <marco.massarotto@mendeley.com>
2015-02-16 09:52:24 +0000

    WL-334 : fixing unit tests

--- test/spec/notifier/notifier.spec.js
+++ test/spec/notifier/notifier.spec.js
@@ -5,14 +5,14 @@ define(function(require) {
     describe('notifier', function() {
     	var notifier = require('notifier');
     	var logger = jasmine.createSpy('logger');
-        notifier.setLogger(logger);
+        var apiNotifier = notifier.createNotifier(logger);
 
 
         it ('should send a notification to the logger', function() {
-            notifier.notify('commWarning', [504, 1, 99], {request: true}, {response: true});
+            apiNotifier.notify('commWarning', [504, 1, 99], {request: true}, {response: true});
             expect(logger).toHaveBeenCalledWith({
             	code: 1001,
-            	level: 'warning',
+            	level: 'warn',
             	message: 'Communication error (status code 504). Retrying (1/99).',
             	request: {request: true},
             	response: {response: true}
