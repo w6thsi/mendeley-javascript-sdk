dabfde554618667f7a22d1c237cf94c2c942b20a
Antonio Greco <antonio.greco@mendeley.com>
2015-04-28 13:53:25 +0100

    WL-177: Adding doc id param and stub spec

---
+++ test/spec/api/annotations.spec.js
@@ -0,0 +1,54 @@
+define(function(require) {
+
+    'use strict';
+
+    require('es5-shim');
+
+    describe('annotations api', function() {
+
+        var api = require('api');
+        var catalogApi = api.catalog;
+        var baseUrl = 'https://api.mendeley.com';
+
+        var mockAuth = require('mocks/auth');
+        api.setAuthFlow(mockAuth.mockImplicitGrantFlow());
+
+        describe('retrieve method', function() {
+            var ajaxSpy;
+            var ajaxRequest;
+            var params = {
+                view: 'all'
+            };
+
+            it('should be defined', function() {
+                expect(typeof catalogApi.retrieve).toBe('function');
+                ajaxSpy = spyOn($, 'ajax').and.returnValue($.Deferred().resolve());
+                catalogApi.retrieve('catalogId', params);
+                expect(ajaxSpy).toHaveBeenCalled();
+                ajaxRequest = ajaxSpy.calls.mostRecent().args[0];
+            });
+
+            it('should use GET', function() {
+                expect(ajaxRequest.type).toBe('GET');
+            });
+
+            it('should use endpoint /catalog/catalogId', function() {
+                expect(ajaxRequest.url).toBe(baseUrl + '/catalog/catalogId');
+            });
+
+            it('should NOT have a Content-Type header', function() {
+                expect(ajaxRequest.headers['Content-Type']).not.toBeDefined();
+            });
+
+            it('should have an Authorization header', function() {
+                expect(ajaxRequest.headers.Authorization).toBeDefined();
+                expect(ajaxRequest.headers.Authorization).toBe('Bearer auth');
+            });
+
+            it('should allow paramaters', function() {
+                expect(ajaxRequest.data).toEqual(params);
+            });
+
+        });
+    });
+});
