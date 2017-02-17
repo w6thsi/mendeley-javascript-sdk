1bfeef4500c91516c8a7fd6e829bdc811c01d4f4
Tomasz Sobczak <tomasz.sobczak@mendeley.com>
2017-02-17 10:37:32 +0000

    allow axios to decide of format of data passed with request

--- test/spec/utilities/utilities.spec.js
+++ test/spec/utilities/utilities.spec.js
@@ -192,75 +192,6 @@ describe('utilities', function() {
                     Accept: 'mime/type1',
                     'Content-Type': 'mime/type2'
                 },
-                data: JSON.stringify(requestData)
-            }, {
-                authFlow: authFlow,
-                followLocation: true
-            });
-        });
-
-        it('should construct the url from supplied pattern and arguments', function() {
-            var requestFunction = utils.requestWithDataFun(assign({
-                method: 'POST',
-                resource: '/test/{first}/{second}',
-                args: ['first', 'second']
-            }, defaultOptions));
-
-            requestFunction(1, 2);
-
-            expect(requestCreateSpy).toHaveBeenCalledWith({
-                method: 'POST',
-                url: 'https://api.mendeley.com/test/1/2',
-                headers: {},
-                data: undefined
-            }, {
-                authFlow: authFlow,
-                followLocation: undefined
-            });
-        });
-
-        it('should allow properties to be filtered from the response using a responseFilter', function(done) {
-            var requestFunction = utils.requestWithDataFun(assign({
-                responseFilter: function(options, response) {
-                    return response.headers;
-                },
-                method: 'POST',
-                resource: '/test'
-            }, defaultOptions));
-
-            requestFunction().then(function(data) {
-                expect(data.Header).toBe('123');
-                done();
-            });
-        });
-
-    });
-
-    describe('requestWithRawDataFun', function() {
-
-        it('should create a request with given properties', function() {
-            var requestData = {
-                id: '123'
-            };
-            var requestFunction = utils.requestWithRawDataFun(assign({
-                method: 'POST',
-                resource: '/test',
-                headers: {
-                    Accept: 'mime/type1',
-                    'Content-Type': 'mime/type2'
-                },
-                followLocation: true
-            }, defaultOptions));
-
-            requestFunction(requestData);
-
-            expect(requestCreateSpy).toHaveBeenCalledWith({
-                method: 'POST',
-                url: 'https://api.mendeley.com/test',
-                headers: {
-                    Accept: 'mime/type1',
-                    'Content-Type': 'mime/type2'
-                },
                 data: requestData
             }, {
                 authFlow: authFlow,
@@ -269,7 +200,7 @@ describe('utilities', function() {
         });
 
         it('should construct the url from supplied pattern and arguments', function() {
-            var requestFunction = utils.requestWithRawDataFun(assign({
+            var requestFunction = utils.requestWithDataFun(assign({
                 method: 'POST',
                 resource: '/test/{first}/{second}',
                 args: ['first', 'second']
@@ -289,7 +220,7 @@ describe('utilities', function() {
         });
 
         it('should allow properties to be filtered from the response using a responseFilter', function(done) {
-            var requestFunction = utils.requestWithRawDataFun(assign({
+            var requestFunction = utils.requestWithDataFun(assign({
                 responseFilter: function(options, response) {
                     return response.headers;
                 },
